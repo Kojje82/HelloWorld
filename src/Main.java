4cb8cba036bc45895611c0d3791ce845a907e48d
carle <carl.edqvist@gmail.com>
2021-12-07 09:58:41 +0100

    Initial commit Added text in printout a second time.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World! We still need to find a new World!");
+        System.out.println("Hello World! We still need to find a new World! Opps it is a few light years away," +
+                " let´s construct a rocket :)");
     }
 
 }
