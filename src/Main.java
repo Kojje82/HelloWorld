736f600f3d74c0b2936a2b4d816f5b9320d6ca7d
carle <carl.edqvist@gmail.com>
2021-12-07 14:50:55 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("A fresh start...or?");
+        System.out.println("What an great idea to write something new.");
+
     }
 
 }
